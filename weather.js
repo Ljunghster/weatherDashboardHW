f348d54cea7ba30c68c997e53a5e526d4fbe2c26
Ljunghster <mpljungh@gmail.com>
2020-05-04 22:37:21 -0700

    key for weather working, not sure which API to use or how to do search

---
+++ weather.js
@@ -0,0 +1,8 @@
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=${document.getElementById(weather).value}&appid=a7ba34611b0e3462cd66466a1b5d8fc6')
+        .then(r => r.json())
+        .then(weather => {
+            document.getElementById('weather').innerHTML = `
+            <h1>${weather.city}</h1>
+            `
+        })
+        .catch(e => console.log(e))
